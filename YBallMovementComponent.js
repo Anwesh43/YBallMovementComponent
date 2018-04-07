cf45093c27215522187db09fc588deb9506dafde
Anwesh43 <anweshthecool0@gmail.com>
2018-04-07 12:19:16 +0530

    adding Animator class

--- YBallMovementComponent.js
+++ YBallMovementComponent.js
@@ -49,4 +49,24 @@ class State {
         }
     }
 }
+
+class Animator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
 customElements.define('yball-movement', YBallMovementComponent)
